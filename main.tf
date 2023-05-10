f2d3145bee2b361d96d12e57dfe1a9d175c7b075
Travis Redfield <travis.redfield@slalom.com>
2023-05-10 12:53:14 -0700

    fix: add http backend

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 # The terraform {} block contains Terraform settings, including the required providers 
 # Terraform will use to provision your infrastructure
 terraform {
+  backend "http" {
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
